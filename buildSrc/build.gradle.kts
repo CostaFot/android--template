bd44eda84eab185da06c0129ccf381c72f302da7
CostaFot <con.fotiadis@gmail.com>
2020-06-01 01:31:17 +0100

    BuildSrc and kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,3 +7,16 @@ repositories {
     google()
     jcenter()
 }
+
+dependencies {
+    /* Example Dependency */
+    /* Depend on the android gradle plugin, since we want to access it in our plugin */
+    implementation("com.android.tools.build:gradle:4.0.0")
+
+    /* Example Dependency */
+    /* Depend on the kotlin plugin, since we want to access it in our plugin */
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    /* Depend on the default Gradle API's since we want to build a custom plugin */
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
