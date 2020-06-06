c2a15600aa9edc90615ae4e025b8dd4bc4171be2
CostaFot <con.fotiadis@gmail.com>
2020-06-06 01:49:44 +0100

    More shenanigans

--- buildSrc/src/main/java/plugins/greet/HelloWorldTask.kt
+++ buildSrc/src/main/java/plugins/greet/HelloWorldTask.kt
@@ -8,6 +8,7 @@ import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkQueue
 import org.gradle.workers.WorkerExecutor
+import java.io.File
 import javax.inject.Inject
 
 @Suppress("UnstableApiUsage")
@@ -24,24 +25,43 @@ open class HelloWorldTask @Inject constructor(
     @TaskAction
     fun run() {
         val workQueue: WorkQueue = workerExecutor.noIsolation()
-        workQueue.submit(WeatherReportWork::class.java) {
+
+        workQueue.submit(HelloReportWork::class.java) {
             message = this@HelloWorldTask.message
             temperature = this@HelloWorldTask.temperature
+            projectDir = project.projectDir
         }
     }
 
-    abstract class WeatherReportWork : WorkAction<WeatherParams> {
+    abstract class HelloReportWork : WorkAction<GreetParams> {
 
-        private val logger = Logging.getLogger(WeatherReportWork::class.simpleName)
+        private val logger = Logging.getLogger(HelloReportWork::class.simpleName)
 
         override fun execute() {
             logger.lifecycle(parameters.message)
             logger.lifecycle("The temperature today is ${parameters.temperature}")
+            logger.lifecycle(parameters.projectDir.absolutePath)
+
+            val builder = StringBuilder()
+            val layoutsDir = File("${parameters.projectDir}/src/main/res/layout")
+            layoutsDir.walkTopDown().filter {
+                it.name.endsWith(".xml")
+            }.forEach {
+                logger.lifecycle(it.name)
+                builder.appendln(it.name)
+            }
+
+            File(parameters.projectDir, "choppa.txt").writeText(builder.toString())
+
+            // read stuff in gitignore file for fun
+            val ff = File(parameters.projectDir, ".gitignore").readText()
+            logger.lifecycle(ff)
         }
     }
 
-    interface WeatherParams : WorkParameters {
+    interface GreetParams : WorkParameters {
         var message: String?
         var temperature: Int?
+        var projectDir: File
     }
 }
